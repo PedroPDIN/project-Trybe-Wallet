2bdf545215a29850bc44c9cd6411e64c69b18be1
Pedro Lima <pedro.oficial1969@gmail.com>
2022-04-25 19:07:29 -0300

    style: adiciona estilização ao header ao componente "Wallet".

--- src/components/login/inputs.jsx
+++ src/components/login/inputs.jsx
@@ -13,7 +13,7 @@ class Inputs extends Component {
             placeholder="email@email.com"
             value={ email }
             onChange={ onInputChange }
-            className="rounded-t-2xl"
+            className="rounded-t-2xl w-80"
           />
 
           <input
@@ -23,7 +23,7 @@ class Inputs extends Component {
             placeholder="***********"
             value={ senha }
             onChange={ onInputChange }
-            className="rounded-b-2xl"
+            className="rounded-b-2xl w-80"
           />
       </div>
     );
