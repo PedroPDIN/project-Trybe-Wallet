c2e0a84c8601748158675b1a70731446be46ffa0
Pedro Lima <pedro.oficial1969@gmail.com>
2022-04-27 11:26:55 -0300

    style: adicona estilização ao componete "Table".

--- src/components/Wallet/table/Table.jsx
+++ src/components/Wallet/table/Table.jsx
@@ -8,34 +8,35 @@ class Table extends Component {
     const { data } = this.props;
     return (
       <div>
-        <table>
+        <table className="flex justify-center border-collapse  
+        text-white mx-2 table-auto">
           <tbody>
-            <tr>
-              <th>Descrição</th>
-              <th>Tag</th>
-              <th>Método de pagamento</th>
-              <th>Valor convertido</th>
-              <th>Moeda</th>
-              <th>Câmbio utilizado</th>
-              <th>Valor Convertido</th>
-              <th>Moeda de conversão</th>
-              <th>Editar/Excluir</th>
+            <tr className='flex flex-row'>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Descrição</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Tag</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Método de pagamento</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Valor convertido</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Moeda</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Câmbio utilizado</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Valor Convertido</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Moeda de conversão</th>
+              <th className='flex items-center justify-center w-44 py-4 border-b border-collapse text-white font-serif'>Excluir</th>
             </tr>
 
             {data.map((element) => (
-              <tr key={ element.id }>
-                <td>{element.description}</td>
-                <td>{element.tag}</td>
-                <td>{element.method}</td>
-                <td>{element.value}</td>
-                <td>{element.exchangeRates[element.currency].name.split('/')[0]}</td>
-                <td>{Number(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
-                <td>
+              <tr key={ element.id } className="flex flex-row">
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{element.description}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{element.tag}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{element.method}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{element.value}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{element.exchangeRates[element.currency].name.split('/')[0]}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>{Number(element.exchangeRates[element.currency].ask).toFixed(2)}</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-400 font-mono'>
                   {Number(element.exchangeRates[element.currency].ask * element.value)
                     .toFixed(2)}
                 </td>
-                <td>Real</td>
-                <td><ButtonExcluir id={ element.id } data-testid="delete-btn" /></td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-cyan-500 font-mono'>Real</td>
+                <td className='flex items-center justify-center w-44 py-2 border-b border-collapse text-red-600 font-mono hover:bg-white active:bg-red-600'><ButtonExcluir id={ element.id } data-testid="delete-btn" /></td>
               </tr>
             ))}
 
